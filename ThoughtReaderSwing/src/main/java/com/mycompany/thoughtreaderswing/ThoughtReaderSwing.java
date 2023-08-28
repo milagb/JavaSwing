0c1a47d80066b26a114af08fd5427ba29faf196c
Camila <camilagbortolotti@gmail.com>
2023-08-28 10:32:58 -0300

    ThoughtReader using Swing java

---
+++ ThoughtReaderSwing/src/main/java/com/mycompany/thoughtreaderswing/ThoughtReaderSwing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thoughtreaderswing;
+
+/**
+ *
+ * @author Camila
+ */
+public class ThoughtReaderSwing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
